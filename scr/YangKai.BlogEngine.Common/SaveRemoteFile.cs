5e5f158733dee1c4084b84a23e155dd54589d597
TossShinHwa <yangkai-13896222@sohu.com>
2013-03-13 22:50:53 +0800

    fix.

--- scr/YangKai.BlogEngine.Common/SaveRemoteFile.cs
+++ scr/YangKai.BlogEngine.Common/SaveRemoteFile.cs
@@ -13,14 +13,17 @@ namespace YangKai.BlogEngine.Common
         /// 是否添加水印
         /// </summary>
         private static readonly bool IsAddWaterMark = false;
+
         /// <summary>
         /// 水印图片相对路径
         /// </summary>
         private static readonly string PicWaterMarkFile = string.Empty;
+
         /// <summary>
         /// 图片保存物理根路径
         /// </summary>
         private static readonly string FileSavePath = HttpContext.Current.Server.MapPath("/");
+
         /// <summary>
         /// 图片保存Web相对路径
         /// </summary>
@@ -28,7 +31,8 @@ namespace YangKai.BlogEngine.Common
 
         public static string SaveContentPic(string content, string fileHeaderName)
         {
-            ContentData cdata = SaveRemoteFileHelper.SavePic(content, IsAddWaterMark, PicWaterMarkFile, FileSavePath, PicSaveWebPath, fileHeaderName);
+            ContentData cdata = SaveRemoteFileHelper.SavePic(content, IsAddWaterMark, PicWaterMarkFile, FileSavePath,
+                                                             PicSaveWebPath, fileHeaderName);
             return cdata.Content;
         }
     }
