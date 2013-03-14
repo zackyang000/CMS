d01e003748330ef48f733b0e93f0b9a0a256f1da
TossShinHwa <yangkai-13896222@sohu.com>
2013-03-14 19:12:59 +0800

    fix.

--- scr/YangKai.BlogEngine.Web.Mvc/Controllers/LabsController.cs
+++ scr/YangKai.BlogEngine.Web.Mvc/Controllers/LabsController.cs
@@ -8,7 +8,7 @@ namespace YangKai.BlogEngine.Web.Mvc.Controllers
 {
     public class LabsController : Controller
     {
-        public FilePathResult ExportDemo()
+        public FilePathResult Export()
         {
             var filepath = Server.MapPath(string.Format("~/excel/{0}.xls", DateTime.Now.ToString("导出数据 yyyy-MM-dd HHmmss")));
            // _labsServices.ExportDemo(filepath);
