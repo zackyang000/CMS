f714506befe99310a3f3dc8e322ff34d8bef7ff6
TossShinHwa <yangkai-13896222@sohu.com>
2013-04-09 22:08:44 +0800

    fix url.

--- scr/YangKai.BlogEngine.Web.Mvc/Common/RouteHelper.cs
+++ scr/YangKai.BlogEngine.Web.Mvc/Common/RouteHelper.cs
@@ -5,62 +5,59 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
 
-namespace YangKai.BlogEngine.Web.Mvc.Common
+/// <summary>
+/// 生成ArticleController中Index和Detail的超链接.
+/// 因为这2个Action的路由中,传值必须要使用groupUrl和channelUrl其中之一.为了便于使用,编写该Helper.
+/// 用于代替Url.Action(actionName, routeValues)
+/// 其效果同于:
+///     Url.Action("Index", new RouteValueDictionary
+///         {
+///             {"Controller", "Article"},
+///             {"Action", "Index"},
+///             {"groupUrl", groupUrl},
+///         });
+/// </summary>
+public class RouteHelper
 {
-    /// <summary>
-    /// 生成ArticleController中Index和Detail的超链接.
-    /// 因为这2个Action的路由中,传值必须要使用groupUrl和channelUrl其中之一.为了便于使用,编写该Helper.
-    /// 用于代替Url.Action(actionName, routeValues)
-    /// 其效果同于:
-    ///     Url.Action("Index", new RouteValueDictionary
-    ///         {
-    ///             {"Controller", "Article"},
-    ///             {"Action", "Index"},
-    ///             {"groupUrl", groupUrl},
-    ///         });
-    /// </summary>
-    public class RouteHelper
+    private UrlHelper Url;
+
+    public RouteHelper(RequestContext requestContext)
     {
-        private UrlHelper Url;
+        Url = new UrlHelper(requestContext);
+    }
 
-        public RouteHelper(RequestContext requestContext)
+    public static RouteHelper Current
+    {
+        get { return new RouteHelper(HttpContext.Current.Request.RequestContext); }
+    }
+
+    public string Index(string groupUrl = "", string channelUrl = "")
+    {
+        return ArticleUrl("Index", groupUrl, channelUrl);
+    }
+
+    public string Detail(string groupUrl)
+    {
+        return groupUrl == string.Empty ? string.Empty : ArticleUrl("Detail", groupUrl, string.Empty);
+    }
+
+    private string ArticleUrl(string actionName, string groupUrl, string channelUrl)
+    {
+        var route = new RouteValueDictionary {{"Controller", "Article"}};
+
+        if (groupUrl != "")
         {
-            Url = new UrlHelper(requestContext);
+            route.Add("groupUrl", groupUrl);
+        }
+        else if (channelUrl != "")
+        {
+            route.Add("channelUrl", channelUrl);
+        }
+        else
+        {
+            throw new ArgumentException("GroupUrl and ChannelUrl must be have one.");
         }
 
-        public static RouteHelper Current
-        {
-            get { return new RouteHelper(HttpContext.Current.Request.RequestContext); }
-        }
-
-        public string Index(string groupUrl = "", string channelUrl = "")
-        {
-            return ArticleUrl("Index", groupUrl, channelUrl);
-        }
-
-        public string Detail(string groupUrl = "", string channelUrl = "")
-        {
-            return ArticleUrl("Detail", groupUrl, channelUrl);
-        }
-
-        private string ArticleUrl(string actionName, string groupUrl = "", string channelUrl = "")
-        {
-            var route = new RouteValueDictionary {{"Controller", "Article"}};
-
-            if (groupUrl != "")
-            {
-                route.Add("groupUrl", groupUrl);
-            }
-            else if (channelUrl != "")
-            {
-                route.Add("channelUrl", channelUrl);
-            }
-            else
-            {
-                throw new ArgumentException("GroupUrl and ChannelUrl must be have one.");
-            }
-
-            return Url.Action(actionName, route);
-        }
+        return Url.Action(actionName, route);
     }
 }
