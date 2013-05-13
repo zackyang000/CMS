0c02ad6a2da0c98f2e8c11545a4de15a35d0ab05
TossShinHwa <yangkai-13896222@sohu.com>
2013-05-13 11:56:23 +0800

    Fix bugs.

--- scr/YangKai.BlogEngine.Infrastructure/Migrations/Configuration.cs
+++ scr/YangKai.BlogEngine.Infrastructure/Migrations/Configuration.cs
@@ -5,7 +5,7 @@ namespace YangKai.BlogEngine.Infrastructure.Migrations
     using System.Data.Entity.Migrations;
     using System.Linq;
 
-    internal sealed class Configuration : DbMigrationsConfiguration<BlogEngineContext>
+    public sealed class Configuration : DbMigrationsConfiguration<BlogEngineContext>
     {
         public Configuration()
         {
