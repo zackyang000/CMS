d72e2052f236e638267e4530f63835d73503b990
TossShinHwa <yangkai-13896222@sohu.com>
2013-05-18 00:05:08 +0800

    Complete home page.

--- scr/YangKai.BlogEngine.Web.Mvc/Controllers/HomeController.cs
+++ scr/YangKai.BlogEngine.Web.Mvc/Controllers/HomeController.cs
@@ -7,78 +7,17 @@ namespace YangKai.BlogEngine.Web.Mvc.Controllers
 {
     public class HomeController : Controller
     {
-
         [ActionName("index")]
         public ActionResult Index()
         {
-            IList<Channel> entities = QueryFactory.Instance.Post.FindAllByNotDeletion();
-            return View(entities);
+            return View();
         }
 
-        [ActionName("layout-header-menu")]
-        [ChildActionOnly]
-        public PartialViewResult Menu()
+        [ActionName("layout-header")]
+        public PartialViewResult Nav()
         {
-            IList<Channel> entities = QueryFactory.Instance.Post.FindAllByNotDeletion();
-            return PartialView(entities);
-        }
-
-        [ActionName("layout-header-caption")]
-        [ChildActionOnly]
-        public PartialViewResult Caption(string channelUrl, string groupUrl)
-        {
-            Channel channel = null;
-            if (!string.IsNullOrEmpty(groupUrl))
-            {
-                channel = QueryFactory.Instance.Post.GetGroup(groupUrl).Channel;
-            }
-            if (!string.IsNullOrEmpty(channelUrl))
-            {
-                channel = QueryFactory.Instance.Post.GetChannel(channelUrl);
-            }
-            return channel == null ? PartialView() : PartialView((object)channel.Description);
-        }
-
-        [ActionName("layout-header-nav")]
-        [ChildActionOnly]
-        public PartialViewResult Nav(string channelUrl, string groupUrl)
-        {
-            IList<Group> entities = null;
-
-            if (!string.IsNullOrEmpty(channelUrl))
-            {
-                entities = QueryFactory.Instance.Post.GetGroupsByChannelUrl(channelUrl);
-                ViewBag.ChannelUrl = channelUrl;
-            }
-            if (!string.IsNullOrEmpty(groupUrl))
-            {
-                entities = QueryFactory.Instance.Post.GetGroupsByGroupUrl(groupUrl);
-                ViewBag.ChannelUrl = QueryFactory.Instance.Post.GetGroup(groupUrl).Channel.Url;
-            }
-
-            if (Request.Url != null) ViewBag.Url = Request.Url.AbsolutePath.Substring(1);
-
-            return entities == null ? PartialView() : PartialView(entities);
-        }
-
-        [ActionName("layout-header-banner")]
-        [ChildActionOnly]
-        public PartialViewResult Banner(string channelUrl, string groupUrl)
-        {
-            string bannerUrl = null;
-
-            if (!string.IsNullOrEmpty(channelUrl))
-            {
-                var channel = QueryFactory.Instance.Post.GetChannel(channelUrl);
-                bannerUrl = channel.BannerUrl;
-            }
-            if (!string.IsNullOrEmpty(groupUrl))
-            {
-                var group = QueryFactory.Instance.Post.GetGroup(groupUrl);
-                bannerUrl = group.Channel.BannerUrl;
-            }
-
-            return PartialView((object)bannerUrl);
+            var channels = QueryFactory.Instance.Post.FindAllByNotDeletion();
+            return PartialView(channels);
         }
     }
 }
