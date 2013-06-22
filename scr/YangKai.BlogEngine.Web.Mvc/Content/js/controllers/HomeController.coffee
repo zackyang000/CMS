3f4f597d86ed3a179915a69faf068fbb175bc5e8
yangkai-13896222@sohu.com <yangkai-13896222@sohu.com>
2013-06-22 10:05:54 +0800

    Refactor.

--- scr/YangKai.BlogEngine.Web.Mvc/Content/js/controllers/HomeController.coffee
+++ scr/YangKai.BlogEngine.Web.Mvc/Content/js/controllers/HomeController.coffee
@@ -1,4 +1,9 @@
 ﻿HomeController=["$scope","$http", ($scope,$http) ->
   $scope.$parent.title='首页'
   $scope.$parent.showBanner=true
+
+  $http.get("/data/words.js").success (data) ->
+    $scope.list = data
+    debugger
+    $scope.$parent.word = $scope.list[0]
   ]
--- scr/YangKai.BlogEngine.Web.Mvc/Content/js/controllers/HomeController.js
+++ scr/YangKai.BlogEngine.Web.Mvc/Content/js/controllers/HomeController.js
@@ -3,6 +3,11 @@
 HomeController = [
   "$scope", "$http", function($scope, $http) {
     $scope.$parent.title = '首页';
-    return $scope.$parent.showBanner = true;
+    $scope.$parent.showBanner = true;
+    return $http.get("/data/words.js").success(function(data) {
+      $scope.list = data;
+      debugger;
+      return $scope.$parent.word = $scope.list[0];
+    });
   }
 ];
