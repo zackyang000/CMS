ca830f6f84fd3ed11872d9b6b18d7273504f4df3
yangkai-13896222@sohu.com <zackyang@outlook.com>
2013-10-26 16:02:09 +0800

    Try to add seo.

--- src/YangKai.BlogEngine.Web.Mvc/Controllers/HomeController.cs
+++ src/YangKai.BlogEngine.Web.Mvc/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using System.Linq;
+using System.Text;
 using System.Web.Mvc;
 using YangKai.BlogEngine.Domain;
 using YangKai.BlogEngine.Service;
@@ -7,10 +8,28 @@ namespace YangKai.BlogEngine.Web.Mvc.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public ActionResult Index(string _escaped_fragment_)
         {
+            //SEO
+            if (!string.IsNullOrEmpty(_escaped_fragment_))
+            {
+                var query = _escaped_fragment_.Split('/');
+                if (query[1] == "post")
+                {
+                    var url = query[2];
+                    var post = Proxy.Repository<Post>().Get(p => p.Url == url);
+                    return Content(post.Content);
+                }
+            }
+
             var channels = Proxy.Repository<Channel>().GetAll(p => !p.IsDeleted).ToList();
             return View(channels);
+        }
+
+        public ActionResult SEO()
+        {
+            var data = Proxy.Repository<Post>().GetAll(p => !p.IsDeleted).OrderByDescending(p => p.CreateDate).ToList();
+            return View(data);
         }
 
         public ActionResult Config()
