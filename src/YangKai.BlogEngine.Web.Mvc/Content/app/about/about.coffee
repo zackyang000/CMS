76adfc8cda37c8dff610c0f911165fb111325676
zackyang@outlook.com <zackyang@outlook.com>
2013-11-09 16:04:06 +0800

    Improve about page.

--- src/YangKai.BlogEngine.Web.Mvc/Content/app/about/about.coffee
+++ src/YangKai.BlogEngine.Web.Mvc/Content/app/about/about.coffee
@@ -4,7 +4,7 @@
 ($routeProvider) ->
   $routeProvider
   .when("/about",
-    templateUrl: "/Content/app/about/about-newegg.tpl.html"
+    templateUrl: "/Content/app/about/about.tpl.html"
     controller: 'AboutCtrl')
 ])
 
@@ -12,9 +12,4 @@
 ["$scope","$translate","$http", ($scope,$translate,$http) ->
   $scope.$parent.title='About'
   $scope.$parent.showBanner=false
-
-  $scope.loading=$translate("global.loading")
-  $http.get("/Content/data/technology.js").success (data) ->
-    $scope.list = data
-    $scope.loading=""
 ])
--- src/YangKai.BlogEngine.Web.Mvc/Content/app/about/about.js
+++ src/YangKai.BlogEngine.Web.Mvc/Content/app/about/about.js
@@ -2,18 +2,13 @@
 angular.module('about', []).config([
   "$routeProvider", function($routeProvider) {
     return $routeProvider.when("/about", {
-      templateUrl: "/Content/app/about/about-newegg.tpl.html",
+      templateUrl: "/Content/app/about/about.tpl.html",
       controller: 'AboutCtrl'
     });
   }
 ]).controller('AboutCtrl', [
   "$scope", "$translate", "$http", function($scope, $translate, $http) {
     $scope.$parent.title = 'About';
-    $scope.$parent.showBanner = false;
-    $scope.loading = $translate("global.loading");
-    return $http.get("/Content/data/technology.js").success(function(data) {
-      $scope.list = data;
-      return $scope.loading = "";
-    });
+    return $scope.$parent.showBanner = false;
   }
 ]);
