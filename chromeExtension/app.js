d3d304adab27f855d25b16385e93b50d0b5cb141
zackyang@outlook.com <zackyang@outlook.com>
2013-12-21 16:54:13 +0800

    init chromeExtension.

---
+++ chromeExtension/app.js
@@ -0,0 +1,11 @@
+
+var kittenGenerator = {
+
+  requestKittens: function() {
+    alert(1);
+  }
+};
+
+document.addEventListener('DOMContentLoaded', function () {
+  kittenGenerator.requestKittens();
+});
