4c867257113ad45daac14dfc812e3e9c24a13cac
zackyang@outlook.com <zackyang@outlook.com>
2014-01-14 15:11:43 +0800

    Use resolve.

--- src/YangKai.BlogEngine.Web.Mvc/Content/app/article/list/article-list.coffee
+++ src/YangKai.BlogEngine.Web.Mvc/Content/app/article/list/article-list.coffee
@@ -3,21 +3,53 @@
 .config(["$routeProvider",
 ($routeProvider) ->
   $routeProvider
-  .when("/list/:channel/:group/:type/:query",
+  .when("/list/:channel/:group/tag/:tag",
     templateUrl: "/Content/app/article/list/article-list.tpl.html"
-    controller: 'ArticleListCtrl')
+    controller: 'ArticleListCtrl'
+    resolve:
+      articles: ['$route','$q','Article',($route,$q,Article)->
+        deferred = $q.defer()
+        Article.queryOnce
+          $filter:"""
+          IsDeleted eq false 
+          and Group/Channel/Url eq '#{$route.current.params.channel}' 
+          and Tags/any(tag:tag/Name eq '#{$route.current.params.tag}')
+          """
+          $skip:($route.current.params.p ? 1)*10 - 10
+        , (data)->
+          deferred.resolve data
+        deferred.promise
+      ]
+  )
   .when("/list/:channel/:group",
     templateUrl: "/Content/app/article/list/article-list.tpl.html"
-    controller: 'ArticleListCtrl')
+    controller: 'ArticleListCtrl'
+    resolve:
+      articles: ['$route','$q','Article',($route,$q,Article)->
+        deferred = $q.defer()
+        Article.queryOnce
+          $filter:"""
+          IsDeleted eq false 
+          and Group/Channel/Url eq '#{$route.current.params.channel}' 
+          and Group/Url eq '#{$route.current.params.group}'
+          """
+          $skip:($route.current.params.p ? 1)*10 - 10
+        , (data)->
+          deferred.resolve data
+        deferred.promise
+      ]
+  )
   .when("/list/:channel",
     templateUrl: "/Content/app/article/list/article-list.tpl.html"
     controller: 'ArticleListCtrl'
     resolve:
       articles: ['$route','$q','Article',($route,$q,Article)->
         deferred = $q.defer()
-        filter='IsDeleted eq false'
         Article.queryOnce
-          $filter:"IsDeleted eq false and Group/Channel/Url eq '#{$route.current.params.channel}'"
+          $filter:"""
+          IsDeleted eq false 
+          and Group/Channel/Url eq '#{$route.current.params.channel}'
+          """
           $skip:($route.current.params.p ? 1)*10 - 10
         , (data)->
           deferred.resolve data
@@ -26,33 +58,34 @@
   )
   .when("/search/:key",
     templateUrl: "/Content/app/article/list/article-list.tpl.html"
-    controller: 'ArticleListCtrl')
+    controller: 'ArticleListCtrl'
+    resolve:
+      articles: ['$route','$q','Article',($route,$q,Article)->
+        deferred = $q.defer()
+        Article.queryOnce
+          $filter:"""
+          IsDeleted eq false 
+          and indexof(Title, '#{$route.current.params.key}') gt -1
+          """
+          $skip:($route.current.params.p ? 1)*10 - 10
+        , (data)->
+          deferred.resolve data
+        deferred.promise
+      ]
+  )
 ])
 
 .controller('ArticleListCtrl',
-["$scope","$window","$translate","$routeParams","$location","Article","articles"
-($scope,$window,$translate,$routeParams,$location,Article,articles) ->
+["$scope","$window","$routeParams","$location","articles"
+($scope,$window,$routeParams,$location,articles) ->
+  scroll(0,0)
   $scope.$parent.showBanner=false
-
-  $scope.$parent.title=$routeParams.group ? $routeParams.channel ? "Search Result '#{$scope.key}'"
-  $scope.currentPage =$routeParams.p ? 1
-
-  $scope.params=
-    channel:$routeParams.channel
-    group:$routeParams.group
-    key:$routeParams.key
-    tag:if $routeParams.type=='tag' then $routeParams.query else ''
+  $scope.$parent.title=$routeParams.tag ? $routeParams.group ? $routeParams.channel ? "Search Result '#{$scope.key}'"
+  $scope.list = articles
+  $scope.params=$routeParams
 
   $scope.setPage = (pageNo) ->
     $location.search({p: pageNo})
-
-    #filter='IsDeleted eq false'
-    #filter+=" and Group/Channel/Url eq '#{$scope.params.channel}'" if $scope.params.channel
-    #filter+=" and Group/Url eq '#{$scope.params.group}'" if $scope.params.group
-    #filter+=" and indexof(Title, '#{$scope.params.key}') gt -1" if $scope.params.key
-    #filter+=" and Tags/any(tag:tag/Name eq '#{$scope.params.tag}')" if $scope.params.tag
-  scroll(0,0)
-  $scope.list = articles
 
   $scope.edit = (item) ->
     $window.location.href="/admin/article('#{item.PostId}')"
--- src/YangKai.BlogEngine.Web.Mvc/Content/app/article/list/article-list.js
+++ src/YangKai.BlogEngine.Web.Mvc/Content/app/article/list/article-list.js
@@ -1,23 +1,52 @@
 ﻿
 angular.module('article-list', ['resource.articles']).config([
   "$routeProvider", function($routeProvider) {
-    return $routeProvider.when("/list/:channel/:group/:type/:query", {
+    return $routeProvider.when("/list/:channel/:group/tag/:tag", {
       templateUrl: "/Content/app/article/list/article-list.tpl.html",
-      controller: 'ArticleListCtrl'
+      controller: 'ArticleListCtrl',
+      resolve: {
+        articles: [
+          '$route', '$q', 'Article', function($route, $q, Article) {
+            var deferred, _ref;
+            deferred = $q.defer();
+            Article.queryOnce({
+              $filter: "IsDeleted eq false \nand Group/Channel/Url eq '" + $route.current.params.channel + "' \nand Tags/any(tag:tag/Name eq '" + $route.current.params.tag + "')",
+              $skip: ((_ref = $route.current.params.p) != null ? _ref : 1) * 10 - 10
+            }, function(data) {
+              return deferred.resolve(data);
+            });
+            return deferred.promise;
+          }
+        ]
+      }
     }).when("/list/:channel/:group", {
       templateUrl: "/Content/app/article/list/article-list.tpl.html",
-      controller: 'ArticleListCtrl'
+      controller: 'ArticleListCtrl',
+      resolve: {
+        articles: [
+          '$route', '$q', 'Article', function($route, $q, Article) {
+            var deferred, _ref;
+            deferred = $q.defer();
+            Article.queryOnce({
+              $filter: "IsDeleted eq false \nand Group/Channel/Url eq '" + $route.current.params.channel + "' \nand Group/Url eq '" + $route.current.params.group + "'",
+              $skip: ((_ref = $route.current.params.p) != null ? _ref : 1) * 10 - 10
+            }, function(data) {
+              return deferred.resolve(data);
+            });
+            return deferred.promise;
+          }
+        ]
+      }
     }).when("/list/:channel", {
       templateUrl: "/Content/app/article/list/article-list.tpl.html",
       controller: 'ArticleListCtrl',
       resolve: {
         articles: [
           '$route', '$q', 'Article', function($route, $q, Article) {
-            var deferred, filter, _ref;
+            var deferred, _ref;
             deferred = $q.defer();
-            filter = 'IsDeleted eq false';
             Article.queryOnce({
-              $filter: "IsDeleted eq false and Group/Channel/Url eq '" + $route.current.params.channel + "'",
+              $filter: "IsDeleted eq false \nand Group/Channel/Url eq '" + $route.current.params.channel + "'",
               $skip: ((_ref = $route.current.params.p) != null ? _ref : 1) * 10 - 10
             }, function(data) {
               return deferred.resolve(data);
@@ -28,28 +57,37 @@ angular.module('article-list', ['resource.articles']).config([
       }
     }).when("/search/:key", {
       templateUrl: "/Content/app/article/list/article-list.tpl.html",
-      controller: 'ArticleListCtrl'
+      controller: 'ArticleListCtrl',
+      resolve: {
+        articles: [
+          '$route', '$q', 'Article', function($route, $q, Article) {
+            var deferred, _ref;
+            deferred = $q.defer();
+            Article.queryOnce({
+              $filter: "IsDeleted eq false \nand indexof(Title, '" + $route.current.params.key + "') gt -1",
+              $skip: ((_ref = $route.current.params.p) != null ? _ref : 1) * 10 - 10
+            }, function(data) {
+              return deferred.resolve(data);
+            });
+            return deferred.promise;
+          }
+        ]
+      }
     });
   }
 ]).controller('ArticleListCtrl', [
-  "$scope", "$window", "$translate", "$routeParams", "$location", "Article", "articles", function($scope, $window, $translate, $routeParams, $location, Article, articles) {
+  "$scope", "$window", "$routeParams", "$location", "articles", function($scope, $window, $routeParams, $location, articles) {
     var _ref, _ref1, _ref2;
+    scroll(0, 0);
     $scope.$parent.showBanner = false;
-    $scope.$parent.title = (_ref = (_ref1 = $routeParams.group) != null ? _ref1 : $routeParams.channel) != null ? _ref : "Search Result '" + $scope.key + "'";
-    $scope.currentPage = (_ref2 = $routeParams.p) != null ? _ref2 : 1;
-    $scope.params = {
-      channel: $routeParams.channel,
-      group: $routeParams.group,
-      key: $routeParams.key,
-      tag: $routeParams.type === 'tag' ? $routeParams.query : ''
-    };
+    $scope.$parent.title = (_ref = (_ref1 = (_ref2 = $routeParams.tag) != null ? _ref2 : $routeParams.group) != null ? _ref1 : $routeParams.channel) != null ? _ref : "Search Result '" + $scope.key + "'";
+    $scope.list = articles;
+    $scope.params = $routeParams;
     $scope.setPage = function(pageNo) {
       return $location.search({
         p: pageNo
       });
     };
-    scroll(0, 0);
-    $scope.list = articles;
     return $scope.edit = function(item) {
       return $window.location.href = "/admin/article('" + item.PostId + "')";
     };
