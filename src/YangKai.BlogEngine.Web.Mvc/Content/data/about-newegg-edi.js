fd4d1d4f984bc2527f2d3a96337dbda739a9574a
zackyang@outlook.com <zackyang@outlook.com>
2014-01-28 10:39:06 +0800

    Edit about page.

---
+++ src/YangKai.BlogEngine.Web.Mvc/Content/data/about-newegg-edi.js
@@ -0,0 +1,114 @@
+﻿[
+    {
+        "ename": "Tony Wang",
+        "cname": "汪健",
+        "desc": "",
+        "email": "Tony.J.Wang@newegg.com",
+        "avatar": "http://apis.newegg.org/common/v1/domain/user/tw14/avatar"
+    },
+    {
+        "ename": "Mark Mu",
+        "cname": "牟晓渝",
+        "desc": "",
+        "email": "Mark.X.Mu@newegg.com",
+        "avatar": "http://apis.newegg.org/common/v1/domain/user/mm67/avatar"
+    },
+    {
+        "ename": "Betty Zheng",
+        "cname": "郑联",
+        "desc": "",
+        "email": "Betty.L.Zheng@newegg.com",
+        "avatar": "http://apis.newegg.org/common/v1/domain/user/bz15/avatar"
+    },
+    {
+        "ename": "Seven Huang",
+        "cname": "黄瀚达",
+        "desc": "",
+        "email": "Seven.H.Huang@newegg.com",
+        "avatar": "http://apis.newegg.org/common/v1/domain/user/sh48/avatar"
+    },
+    {
+        "ename": "Steven Shao",
+        "cname": "邵勇",
+        "desc": "",
+        "email": "Steven.Y.Shao@newegg.com",
+        "avatar": "http://apis.newegg.org/common/v1/domain/user/ss39/avatar"
+    },
+    {
+        "ename": "Brent Cheng",
+        "cname": "程翔",
+        "desc": "",
+        "email": "Brent.X.Cheng@newegg.com",
+        "avatar": "http://apis.newegg.org/common/v1/domain/user/bc78/avatar"
+    },
+    {
+        "ename": "Peter Xia",
+        "cname": "夏昌",
+        "desc": "",
+        "email": "Peter.C.Xia@newegg.com",
+        "avatar": "http://apis.newegg.org/common/v1/domain/user/px56/avatar"
+    },
+    {
+        "ename": "Terry Ren",
+        "cname": "任钰",
+        "desc": "",
+        "email": "Terry.Y.Ren@newegg.com",
+        "avatar": "http://apis.newegg.org/common/v1/domain/user/tr29/avatar"
+    },
+    {
+        "ename": "Bing Yan",
+        "cname": "晏斌",
+        "desc": "",
+        "email": "Bing.B.Yan@newegg.com",
+        "avatar": "http://apis.newegg.org/common/v1/domain/user/by52/avatar"
+    },
+    {
+        "ename": "Sam Hu",
+        "cname": "胡民",
+        "desc": "",
+        "email": "Sam.M.Hu@newegg.com",
+        "avatar": "http://apis.newegg.org/common/v1/domain/user/sh17/avatar"
+    },
+    {
+        "ename": "Frank Wang",
+        "cname": "王澍",
+        "desc": "",
+        "email": "Frank.S.Wang@newegg.com",
+        "avatar": "http://apis.newegg.org/common/v1/domain/user/fw70/avatar"
+    },
+    {
+        "ename": "Banning Liu",
+        "cname": "刘邦林",
+        "desc": "",
+        "email": "Banning.B.Liu@newegg.com",
+        "avatar": "http://apis.newegg.org/common/v1/domain/user/bl35/avatar"
+    },
+    {
+        "ename": "Randy Chen",
+        "cname": "陈玉麟",
+        "desc": "",
+        "email": "Randy.Y.Chen@newegg.com",
+        "avatar": "http://apis.newegg.org/common/v1/domain/user/rc27/avatar"
+    },
+    {
+        "ename": "Ruby Wang",
+        "cname": "王娇杨",
+        "desc": "",
+        "email": "Ruby.J.Wang@newegg.com",
+        "avatar": "http://apis.newegg.org/common/v1/domain/user/rw38/avatar"
+    },
+    {
+        "ename": "Andre Shen",
+        "cname": "沈威",
+        "desc": "",
+        "email": "Andre.W.Shen@newegg.com",
+        "avatar": "http://apis.newegg.org/common/v1/domain/user/as13/avatar"
+    },
+    {
+        "ename": "Gavin.Li",
+        "cname": "李靖",
+        "desc": "",
+        "email": "Gavin.J.Li@newegg.com",
+        "avatar": "http://apis.newegg.org/common/v1/domain/user/gl25/avatar"
+    }
+]
