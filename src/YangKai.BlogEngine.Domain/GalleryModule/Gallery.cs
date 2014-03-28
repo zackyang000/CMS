ce39faeac54a95da8592acfc2456ecb87e172704
zackyang@outlook.com <zackyang@outlook.com>
2014-03-28 11:20:43 +0800

    Remove useless elm.

--- src/YangKai.BlogEngine.Domain/GalleryModule/Gallery.cs
+++ src/YangKai.BlogEngine.Domain/GalleryModule/Gallery.cs
@@ -13,6 +13,7 @@ namespace YangKai.BlogEngine.Domain
         public string Name { get; set; }
         public string Description { get; set; }
         public string Cover { get; set; }
+        public bool IsHidden { get; set; }
 
         public virtual List<Photo> Photos { get; set; } 
     }
