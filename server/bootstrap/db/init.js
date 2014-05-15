bc628bb9be9839af39d1c86974a8e2f9199a8294
zackyang@outlook.com <zackyang@outlook.com>
2014-05-15 17:32:39 +0800

    comment test data.

--- server/bootstrap/db/init.js
+++ server/bootstrap/db/init.js
@@ -11,10 +11,10 @@ var initData = function(model, path) {
 
 module.exports = function() {
   //initData(mongoose.model('User'), '/bootstrap/db/system/user.json');
-  initData(mongoose.model('Article'), '/bootstrap/db/article/article.json');
-  initData(mongoose.model('Category'), '/bootstrap/db/article/category.json');
-  initData(mongoose.model('Comment'), '/bootstrap/db/common/comment.json');
-  initData(mongoose.model('Tag'), '/bootstrap/db/common/tag.json');
-  initData(mongoose.model('Gallery'), '/bootstrap/db/photo/gallery.json');
-  initData(mongoose.model('Photo'), '/bootstrap/db/photo/photo.json');
+  //initData(mongoose.model('Article'), '/bootstrap/db/article/article.json');
+  //initData(mongoose.model('Category'), '/bootstrap/db/article/category.json');
+  //initData(mongoose.model('Comment'), '/bootstrap/db/common/comment.json');
+  //initData(mongoose.model('Tag'), '/bootstrap/db/common/tag.json');
+  //initData(mongoose.model('Gallery'), '/bootstrap/db/photo/gallery.json');
+  //initData(mongoose.model('Photo'), '/bootstrap/db/photo/photo.json');
 }
