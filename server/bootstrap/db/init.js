0c54d697f3584366ff16c11daa5364251bd2b23f
Zack <zackyang@outlook.com>
2014-07-06 22:37:12 +0800

    Edit init db.

--- server/bootstrap/db/init.js
+++ server/bootstrap/db/init.js
@@ -1,6 +1,7 @@
 var fs = require("fs");
 var appPath = process.cwd();
 var mongoose = require('mongoose');
+var User = mongoose.model('User');
 
 var initData = function(model, path) {
   require(appPath + path).forEach(function(item) {
@@ -11,11 +12,15 @@ var initData = function(model, path) {
 }
 
 module.exports = function() {
-  //initData(mongoose.model('User'), '/bootstrap/db/system/user.json');
-  //initData(mongoose.model('Article'), '/bootstrap/db/article/article.json');
-  //initData(mongoose.model('Category'), '/bootstrap/db/article/category.json');
-  //initData(mongoose.model('Comment'), '/bootstrap/db/common/comment.json');
-  //initData(mongoose.model('Tag'), '/bootstrap/db/common/tag.json');
-  //initData(mongoose.model('Gallery'), '/bootstrap/db/photo/gallery.json');
-  //initData(mongoose.model('Photo'), '/bootstrap/db/photo/photo.json');
+  User.find().exec(function(err, users) {
+      if(users.length==0){
+        initData(mongoose.model('User'), '/bootstrap/db/system/user.json');
+        initData(mongoose.model('Article'), '/bootstrap/db/article/article.json');
+        initData(mongoose.model('Category'), '/bootstrap/db/article/category.json');
+        initData(mongoose.model('Comment'), '/bootstrap/db/common/comment.json');
+        initData(mongoose.model('Tag'), '/bootstrap/db/common/tag.json');
+        initData(mongoose.model('Gallery'), '/bootstrap/db/photo/gallery.json');
+        initData(mongoose.model('Photo'), '/bootstrap/db/photo/photo.json');
+      }
+  });
 }
