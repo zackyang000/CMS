e2045000f0ed5b2b14567267492ef98b7a872cb6
Zack Yang <zackyang@outlook.com>
2014-07-09 10:42:42 +0800

    Edit.

--- chrome-extension/content_script.js
+++ chrome-extension/content_script.js
@@ -1,30 +1,24 @@
-﻿//var targetSite="http://www.woshinidezhu.com"
-//var targetSite="http://localhost:33333"
+﻿var server = "http://www.woshinidezhu.com"
+var client = "http://www.woshinidezhu.com"
 
-var server="http://www.woshinidezhu.com"
-var client="http://www.woshinidezhu.com"
-
-try
-{
-  var data=readability.get()
+try {
+  var data = readability.get()
   $("body").html('<div class="backend_loadingbox">Processing...</div>')
-  if (data.description.length>200)
-    data.description=data.description.substring(0,200)+"...";
+  if(data.description.length > 200)
+    data.description = data.description.substring(0, 200) + "...";
 
-  $.post(server+"/odata/Article",
+  $.post(server + "/odata/Article",
     {
       Content: data.content,
-      Description: data.description.substring(0,200),
+      Description: data.description.substring(0, 200),
       Title: data.title,
       Source: document.URL
     },
-    function(data){
-      window.location.href=client+"/admin/article('"+data.PostId+"')";
+    function(data) {
+      window.location.href = client + "/admin/article('" + data.PostId + "')";
     });
-
 }
-catch(e)
-{
+catch (e) {
   alert("解析失败");
   window.location.reload();
 }
