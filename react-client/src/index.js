4ef91d03158d63466d5bdf9f3e3e9c61935251c5
Zack Yang <zackyang@outlook.com>
2015-09-06 14:33:28 +0800

    add index.js

---
+++ react-client/src/index.js
@@ -0,0 +1,34 @@
+import './index.scss';
+import 'babel-core/polyfill';
+import React from 'react';
+import { Provider } from 'react-redux';
+import { Router } from 'react-router';
+import { DevTools, DebugPanel, LogMonitor } from 'redux-devtools/lib/react';
+import createStore from './onekit/store/store';
+import createRoutes from './routes';
+import reducers from './app/reducers';
+import config from './config';
+
+const store = createStore(reducers, config);
+
+if (!config.isProduction && module.hot) {
+  module.hot.accept('./app/reducers', () => {
+    const nextRootReducer = require('./app/reducers');
+    store.replaceReducer(nextRootReducer);
+  });
+}
+
+const routes = createRoutes();
+
+React.render(
+  <div>
+    <Provider store={store}>
+      { () => <Router children={routes} /> }
+    </Provider>
+    { config.enableDevTools &&
+      <DebugPanel top right bottom>
+        <DevTools store={store} monitor={LogMonitor} />
+      </DebugPanel>
+    }
+  </div>
+, document.getElementById('root'));
