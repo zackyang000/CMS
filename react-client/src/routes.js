628194980064dd4908216b5a356c32f8ff358cf5
Zack Yang <zackyang@outlook.com>
2015-09-25 18:46:04 +0800

    remove warp

--- react-client/src/routes.js
+++ react-client/src/routes.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
 import { Route, IndexRoute } from 'react-router';
-import warp from './onekit/helpers/wrapComponent';
 import menus from './menus';
 import App from './onekit/containers/App';
 import Home from './app/home/containers/Home';
 
 export default function() {
   return (
-    <Route component={warp(App, { menus })}>
+    <Route component={App}>
       <Route path="/" component={Home} />
     </Route>
   );
