29bcacbe4bf458e943013ff03c051c654980bb98
Zack Yang <zackyang@outlook.com>
2015-09-29 23:12:19 +0800

    add page.

---
+++ react-client/src/app/articles/containers/Articles.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router';
+import * as actions from '../redux/articles';
+import ArticlesList from '../components/ArticlesList';
+
+@connect(state => state.articles, actions)
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+    this.props.fetchArticles();
+  }
+
+  render() {
+    const { articles } = this.props;
+    return (
+      <div>
+        <div className="content">
+          <ArticlesList articles={articles} />
+        </div>
+      </div>
+    );
+  }
+}
+
