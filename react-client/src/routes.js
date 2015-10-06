b59c3d5a6b43230296a95400c90c8b22fb925220
Zack Yang <zackyang@outlook.com>
2015-10-06 22:39:58 +0800

    add new route

--- react-client/src/routes.js
+++ react-client/src/routes.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 import { Route, IndexRoute } from 'react-router';
 import App from './app/framework/containers/App';
 import Home from './app/home/containers/Home';
+import Articles from './app/articles/containers/Articles';
 
 export default function() {
   return (
     <Route component={App}>
       <Route path="/" component={Home} />
+      <Route path="/articles" component={Articles} />
     </Route>
   );
 }
