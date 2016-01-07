26e43816b61cdff7b3bab67b42f9c4eb0a706824
Zack Yang <zackyang@outlook.com>
2016-01-07 17:13:24 +0800

    improve router

--- react-native/src/app/routes.js
+++ react-native/src/app/routes.js
@@ -1,13 +1,37 @@
 import App from './framework/App';
 import Home from './home/containers/Home';
 
-export default function() {
-  return {
-    defaultRoute: function() {
-      return {
-        name: 'Home',
-        component: Home,
-      }
-    }
+export default class Routes {
+  static initialRoute = {
+    name: 'Home',
+    component: Home,
   };
+
+  constructor(navigator) {
+    this.navigator = navigator;
+  }
+
+  push(route) {
+    let currentRoute = this.getCurrentRoute();
+    if (route.id !== currentRoute.id) {
+      this.navigator.push(route);
+    }
+  }
+
+  getCurrentRoute() {
+    let routeList = this.navigator.getCurrentRoutes();
+    return routeList[routeList.length - 1];
+  }
+
+  isCurrentRoute(routeId) {
+    return routeId === getCurrentRoute().id;
+  }
+
+  toHome() {
+    this.push({
+      id: 'home',
+      title: '最新',
+      component: Home
+    });
+  }
 }
