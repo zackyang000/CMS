67ecbd04c730af8966ec7694086a77d98eb927cc
Zack Yang <zackyang@outlook.com>
2016-01-08 11:06:15 +0800

    add action.

---
+++ react-native/src/app/home/redux/post.js
@@ -0,0 +1,24 @@
+import { createReducer } from 'cuz';
+
+const FETCH_POSTS = 'fetch posts';
+
+export function fetchPosts() {
+  return {
+    type: FETCH_POSTS,
+    promise: (request) => request.get(`/articles?$top=10&$select=title`)
+  };
+}
+
+const initState = {
+  posts: [],
+}
+
+export default createReducer(initState, {
+  [FETCH_POSTS](state, action) {
+    return {
+      ...state,
+      posts: action.value,
+    }
+  }
+});
+
--- react-native/src/config.js
+++ react-native/src/config.js
@@ -1,17 +1,4 @@
-require('babel/polyfill');
-
-var environment = {
-  development: {
-    apiRoot: 'http://local.xxx.alibaba-inc.com',
-  },
-  testing: {
-    apiRoot: 'http://test.xxx.alibaba-inc.com',
-  },
-  production: {
-    apiRoot: 'http://xxx.alibaba-inc.com',
-  }
-}[process.env.NODE_ENV || 'development'];
-
 module.exports = { 
-  platform: 'mobile'
+  platform: 'mobile',
+  apiRoot: 'http://api.zackyang.com/oData',
 };
