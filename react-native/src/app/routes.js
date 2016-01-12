195ec88d46ff418eb9f61e24d01f3f7f3e79dc3b
Zack Yang <zackyang@outlook.com>
2016-01-12 17:44:56 +0800

    make Route like react-router

--- react-native/src/app/routes.js
+++ react-native/src/app/routes.js
@@ -1,33 +1,15 @@
+import React from 'react-native';
+import { Route } from 'cuz';
 import Home from './home/containers/Home';
 import PostList from './post/containers/List';
 import PostDetail from './post/containers/Detail';
 
-export default class Routes {
-  static initialRoute = {
-    name: '首页',
-    component: Home,
-  };
-
-  // <Route path="world" component={World} />
-
-  static routes = {
-    home: Home,
-    post: PostList,
-    'post/detail': PostDetail,
-  };
-
-  constructor(navigator) {
-    this.navigator = navigator;
-  }
-
-  push(name, params) {
-    this.navigator.push({
-      component: Routes.routes[name],
-      ...params
-    });
-  }
-
-  pop() {
-    this.navigator.pop();
-  }
+export default function() {
+  return (
+    <Route>
+      <Route path="home" component={Home} />
+      <Route path="post" component={PostList} />
+      <Route path="post/detail" component={PostDetail} />
+    </Route>
+  );
 }
