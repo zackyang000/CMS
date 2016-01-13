e989192367bc4c37036a257f32141645f15fee39
Zack Yang <zackyang@outlook.com>
2016-01-13 10:17:11 +0800

    cms to app.

--- react-native/ios/App/AppDelegate.m
+++ react-native/ios/App/AppDelegate.m
@@ -31,7 +31,7 @@
    * on the same Wi-Fi network.
    */
 
-  jsCodeLocation = [NSURL URLWithString:@"http://10.16.217.45:8081/index.ios.bundle?platform=ios&dev=true"];
+  jsCodeLocation = [NSURL URLWithString:@"http://localhost:8081/index.ios.bundle?platform=ios&dev=true"];
 
   /**
    * OPTION 2
@@ -42,7 +42,7 @@
 //   jsCodeLocation = [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
 
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
-                                                      moduleName:@"CMS"
+                                                      moduleName:@"App"
                                                initialProperties:nil
                                                    launchOptions:launchOptions];
 
--- react-native/ios/AppTests/AppTests.m
+++ react-native/ios/AppTests/AppTests.m
@@ -16,11 +16,11 @@
 #define TIMEOUT_SECONDS 240
 #define TEXT_TO_LOOK_FOR @"Welcome to React Native!"
 
-@interface CMSTests : XCTestCase
+@interface AppTests : XCTestCase
 
 @end
 
-@implementation CMSTests
+@implementation AppTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
