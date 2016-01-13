100ca208c7af0476f523527f7ae6fecf63cb2a7d
Zack Yang <zackyang@outlook.com>
2016-01-14 00:03:50 +0800

    refacting.

--- react-native/src/index.js
+++ react-native/src/index.js
@@ -3,6 +3,7 @@ import config from './config';
 import reducers from './app/reducers';
 import routes from './app/routes';
 
+console.log(cuz)
 export default function() {
   return cuz(reducers, routes, config); 
 }
