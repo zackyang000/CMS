cce0ae4f64f6d871fbd0a2895babda8c0e3c9f97
Zack Yang <zackyang@outlook.com>
2016-01-14 09:41:01 +0800

    remove console.log

--- react-native/src/index.js
+++ react-native/src/index.js
@@ -3,7 +3,6 @@ import config from './config';
 import reducers from './app/reducers';
 import routes from './app/routes';
 
-console.log(cuz)
 export default function() {
   return cuz(reducers, routes, config); 
 }
