576cc681234d5bc4f764b22c8eeca3f00689fd41
Zack Yang <zackyang@outlook.com>
2016-02-15 21:21:32 +0800

    refactor(server): Moving to ES6 from CoffeeScript.

--- new-server/models/article/category.coffee
+++
@@ -1,7 +0,0 @@
-module.exports =
-  url: String
-  name: Object
-  description: String
-  main: Boolean
-  order: Number
-  disable: Boolean
---
+++ new-server/models/article/category.js
@@ -0,0 +1,8 @@
+module.exports = {
+  url: String,
+  name: Object,
+  description: String,
+  main: Boolean,
+  order: Number,
+  disable: Boolean,
+};
