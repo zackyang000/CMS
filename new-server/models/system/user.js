0339002ec6925e935c3fc63455fb85c7486fe057
Zack Yang <zackyang@outlook.com>
2016-02-17 21:33:25 +0800

    refactor(server): Moving to ES6 from CoffeeScript.

---
+++ new-server/models/system/user.js
@@ -0,0 +1,14 @@
+module.exports = {
+  name: String,
+  loginName: String,
+  password: {
+    type: String,
+    select: false,
+  },
+  email: String,
+  token: {
+    type: String,
+    select: false,
+  },
+  disabled: Boolean,
+};
