9d0dc6b11856a52411b2a4a78df5121ee19b72e6
Zack Yang <zackyang@outlook.com>
2016-02-28 23:11:01 +0800

    refactor(server): Moving to ES6 from CoffeeScript.

--- new-server/src/functions/login.js
+++ new-server/src/functions/login.js
@@ -1,20 +1,24 @@
-import crypto from "crypto";
-import { Function as func } from 'node-odata';
-import { resources } from 'node-odata';
+import crypto from 'crypto';
+import { Function as func, resources } from 'node-odata';
 
-router = func();
+const router = func();
 
-router.post('/login', (req, res, next) => {
-  name = req.body.name;
-  pwd = req.body.password;
-  resources.user.findOne({password: pwd}).or([{ loginName: name }, { email: name }]).exec((err, user) => {
+router.post('/login', (req, res) => {
+  const name = req.body.name;
+  const pwd = req.body.password;
+  resources.user.findOne({
+    password: pwd,
+  }).or([
+    { loginName: name },
+    { email: name },
+  ]).exec((err, user) => {
     if (!user) {
-      return res.send(401, "Failed to login.");
+      return res.send(401, 'Failed to login.');
     }
-    user.token = crypto.createHash("md5").update(new Date() + pwd).digest("hex");
+    user.token = crypto.createHash('md5').update(new Date() + pwd).digest('hex');
     user.save();
-    res.set("authorization", user.token);
-    res.json({
+    res.set('authorization', user.token);
+    return res.json({
       name: user.name,
       loginName: user.loginName,
       email: user.email,
@@ -24,11 +28,11 @@ router.post('/login', (req, res, next) => {
   });
 });
 
-router.post('/auto-login', (req, res, next) => {
-  if (!user) {
-    return res.send(401, "Failed to auto-login.");
+router.post('/auto-login', (req, res) => {
+  if (!req.user) {
+    return res.send(401, 'Failed to auto-login.');
   }
-  res.json({
+  return res.json({
     name: req.user.name,
     loginName: req.user.loginName,
     email: req.user.email,
@@ -37,15 +41,15 @@ router.post('/auto-login', (req, res, next) => {
   });
 });
 
-router.post('/logoff', (req, res, next) => {
-  token = req.get("authorization");
-  resources.user.findOne(token: token).exec((err, user) => {
+router.post('/logoff', (req, res) => {
+  const token = req.get('authorization');
+  resources.user.findOne({ token }).exec((err, user) => {
     if (!user) {
-      return res.send(400, "User not found.")
+      return res.send(400, 'User not found.');
     }
     user.token = null;
     user.save();
-    res.json({
+    return res.json({
       name: user.name,
       loginName: user.loginName,
       email: user.email,
