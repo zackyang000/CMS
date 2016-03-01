b4ded6e22553b54db0f696eec36ae8d5d231b7ed
Zack Yang <zackyang@outlook.com>
2016-03-01 22:22:31 +0800

    refactor(server): Moving to ES6 from CoffeeScript.

--- new-server/src/middleware/authorization.js
+++ new-server/src/middleware/authorization.js
@@ -1,12 +1,12 @@
 import { resources } from 'node-odata';
 
 module.exports = (req, res, next) => {
-  token = req.headers.authorization;
+  const token = req.headers.authorization;
   if (!token) {
     return next();
   }
-  resources.user.findOne({token: token}).exec((err, user) => {j
-    if(user && !user.disabled) {
+  return resources.user.findOne({ token }).exec((err, user) => {
+    if (user && !user.disabled) {
       req.user = user;
     }
     return next();
