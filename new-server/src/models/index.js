475c52fc3bd109b169bcc266e2069fbec3ba3751
Zack Yang <zackyang@outlook.com>
2016-03-07 23:16:50 +0800

    refactor(server): Moving to ES6 from CoffeeScript.

--- new-server/src/models/index.js
+++ new-server/src/models/index.js
@@ -7,11 +7,11 @@ import gallery from './gallery/gallery';
 import user from './system/user';
 
 module.exports = {
-  article: require('./article/article')
-  comment: require('./article/comment')
-  category: require('./article/category')
-  board: require('./board/board')
-  tag: require('./article/tag')
-  gallery: require('./gallery/gallery')
-  user: require('./system/user')
-}
+  article,
+  comment,
+  category,
+  board,
+  tag,
+  gallery,
+  user,
+};
