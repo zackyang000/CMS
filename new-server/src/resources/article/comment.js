69607514cecf9f115a75da983c7002301dd9dbdd
Zack Yang <zackyang@outlook.com>
2016-03-10 23:21:11 +0800

    refactor(server): Moving to ES6 from CoffeeScript.

--- new-server/src/resources/article/comment.js
+++ new-server/src/resources/article/comment.js
@@ -1,10 +1,10 @@
-import { Resource } from 'node-odata'
-import model from '../../models'
-import auth from '../auth'
+import { Resource } from 'node-odata';
+import model from '../../models';
+import auth from '../auth';
 
 module.exports = Resource('comment', model.comment)
 .orderBy('date desc')
 .put()
   .auth(auth.admin)
 .delete()
-  .auth(auth.admin)
+  .auth(auth.admin);
